4228f766a1987f7e62a40b515e92581ca26af884
Kristoffer Walker <kixxauth@gmail.com>
2010-12-27 20:37:17 -0500

    Start testing micro-templating.template() function.
     On branch kris
    
    	modified:   lib/future/micro-templating.js
        modified:   tests/test-micro-templating.js

--- tests/test-micro-templating.js
+++ tests/test-micro-templating.js
@@ -31,5 +31,50 @@
 dump('\n');
 console.log('starting', __url__);
 
-var path = require('future/path');
+var mtpl = require('future/micro-templating');
+
+exports.invalid_params = function (test) {
+    test.assertRaises(
+          function () {
+              var x = mtpl.template();
+          }
+        , 'typeof template str [undefined] !== [string]'
+        , '.template() throws'
+        );
+    test.assertRaises(
+          function () {
+              var x = mtpl.template({});
+          }
+        , 'typeof template str [object] !== [string]'
+        , '.template({}) throws'
+        );
+    test.assertRaises(
+          function () {
+              var x = mtpl.template(null);
+          }
+        , 'typeof template str [object] !== [string]'
+        , '.template(null) throws'
+        );
+    test.assertRaises(
+          function () {
+              var x = mtpl.template([]);
+          }
+        , 'typeof template str [object] !== [string]'
+        , '.template(null) throws'
+        );
+    test.assertRaises(
+          function () {
+              var x = mtpl.template(false);
+          }
+        , 'typeof template str [boolean] !== [string]'
+        , '.template(null) throws'
+        );
+};
+
+exports.returns_function = function (test) {
+    test.assertEqual( typeof mtpl.template('')
+                    , 'function'
+                    , 'typeof .template("") == "function"'
+                    );
+};
 
