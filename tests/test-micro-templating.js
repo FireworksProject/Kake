ade64150ba91abe7fb8d1b19c720dffd6506d602
Kristoffer Walker <kixxauth@gmail.com>
2010-12-29 15:54:18 -0500

    Comment updates in test files.
     On branch master
    
    	modified:   tests/test-micro-templating.js
    	modified:   tests/test-path.js

--- tests/test-micro-templating.js
+++ tests/test-micro-templating.js
@@ -6,10 +6,6 @@
  * Some rights are reserved, but licensed to you under the MIT license.
  * See MIT-LICENSE or http://opensource.org/licenses/mit-license.php
  * for more information.
- * 
- * Prior art:
- * John Resig - http://ejohn.org/ - MIT Licensed
- * Rick Strahl - http://www.west-wind.com/Weblog/posts/509108.aspx
  */
 
 /*jslint
@@ -26,7 +22,7 @@
 , strict: false
 */
 
-/*global require: false, exports: true, console: false */
+/*global require: false, exports: true, console: false, dump: false */
 
 dump('\n');
 console.log('starting', __url__);
